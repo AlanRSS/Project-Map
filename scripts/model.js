01c524195ce95f99ffd06cd828389b540a722f2b
Alan Santos <alanrss_1@hotmail.com>
2016-02-12 12:37:41 +0100

    Big Update
    added a lot of functionality, search implemented, info windows and
    Pulling api info pictures from Flickr

--- scripts/model.js
+++ scripts/model.js
@@ -1,29 +1,109 @@
+
 var ViewModel = function() {
 	var self = this;
-
-	self.locations = ko.observableArray( [] );
-	self.expanded = ko.observable( false );
+	//initializing the variables
+	self.locations = ko.observableArray();
+	setTimeout(function() {self.locations(allMarkers);}, 4000); 
 	self.searchVal = ko.observable();
-
-	self.slide = function() {
-		
-		
-	};
-	//method binded to clear the map of markers
+	
+	//method bound to clear the map 
 	self.hideMarkers = function(){
-		clearMarkers();
+		self.searchVal("");
+		self.FilterB();
 	};
 
-	//this method checks if the string of the title of the location contains the elements of the query
-	self.Filter = function() {
-		clearMarkers();
-		currentMarkers=[];
-	for (var i = length - 1; i >= 0; i--) {
-		if(markers[i].title.toLowerCase().indexOf(self.searchVal().toLowerCase()) >= 0) {
-			currentMarkers.push(markers[i]);
+	//this method checks if the string of the title of the location contains the elements of the query using a button
+	
+	self.FilterB = function(){
+		if(self.searchVal() != undefined){
+			clearMarkers();
+			currentMarkers=[];
+			for (var i = length - 1; i >= 0; i--){
+				if(allMarkers[i].title.toLowerCase().indexOf(self.searchVal().toLowerCase()) >= 0) {
+					allMarkers[i].setMap(map);
+					currentMarkers.push(allMarkers[i]);
+	    		}
+			}
+			self.locations(currentMarkers);
+			loc = self.locations;
 		}
-	}
-	setMapOn(map);
+	};
+	//this method dinamically updates the markers as the user inputs the search term, I inserted both methods just to show the functionality but only one should be used as it is redundant
+	self.Filter = ko.pureComputed({
+        read: function () {
+            return self.searchVal();
+        },
+        write: function (value) {
+           if(value != undefined){
+			clearMarkers();
+			currentMarkers=[];
+			for (var i = length - 1; i >= 0; i--){
+				if(allMarkers[i].title.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+					allMarkers[i].setMap(map);
+					currentMarkers.push(allMarkers[i]);
+	    		}
+			}
+			self.locations(currentMarkers);
+			loc = self.locations;
+            }
+        },
+        
+    });
+
+
+ addMarkerWithTimeout = function(position, title, content, timeout) {
+	window.setTimeout(function() {
+    	
+ 
+    	var marker = (new google.maps.Marker({
+			position: position,
+			map: map,
+			animation: google.maps.Animation.DROP,
+			title: title,
+			content: content
+		}));
+
+		
+		infoWindow = new google.maps.InfoWindow({
+    		content: null ,
+    		maxWidth: 500,
+    		maxHeight: 200
+ 		});
+		marker.addListener('click', toggleBounce);
+		marker.addListener('click', self.info);
+  		allMarkers.push(marker);
+	}, timeout);
 }
+
+
+	self.info = function() {
+
+		console.log(this);
+		var title = this.title;
+		$.ajax({
+			url:'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1b5ad63f3251fe5249199e1a3d291987&text=%22'+ title +'%22&format=json&nojsoncallback=1',
+			async: 'false',
+			success: function(data){ 
+				console.log(data);
+				var contentStr = "";
+				if (data.photos.photo.length >= 3){
+					for (var i = 2; i >= 0; i--) {
+						contentStr += '<div class = "imagebox"><img class="img-responsive" src= "https://farm' + data.photos.photo[i].farm + '.staticflickr.com/' + data.photos.photo[i].server + '/' + data.photos.photo[i].id + '_' + data.photos.photo[i].secret + '.jpg"></div>';
+					}
+				}
+				else{
+				contentStr += '<img class="img-responsive" BORDER="0" ALIGN="Left" src= "https://farm' + data.photos.photo[0].farm + '.staticflickr.com/' + data.photos.photo[0].server + '/' + data.photos.photo[0].id + '_' + data.photos.photo[0].secret + '.jpg">';
+				}
+
+				
+				infoWindow.setContent(contentStr);
+			}
+		});
+
+		//var content = '<IMG BORDER="0" ALIGN="Left" src= "https://farm' + this.content.responseJSON.photos.photo[0].farm + '.staticflickr.com/' + this.content.responseJSON.photos.photo[0].server + '/' + this.content.responseJSON.photos.photo[0].id + '_' + this.content.responseJSON.photos.photo[0].secret + '.jpg">';
+		//infoWindow.setContent(this.content);
+		infoWindow.open(map, this);	
+	}	
 };
+
 ko.applyBindings( new ViewModel());
--- scripts/script.js
+++ scripts/script.js
@@ -1,74 +1,138 @@
 //Collection of locations to be initialized when the map runs
 var MyLocations = [
-{
-	latitude : -8.811645,
-	longitude: 13.230324,
-	title: "Rialto"
 
-},
 {
 	latitude: -8.81495,
 	longitude: 13.234965,
-	title: "Epic Sana"
+	title: "Epic Sana Luanda Hotel ",
+	description: "Epic Sana Luanda",
+	//content: content("Epic Sana Luanda Hotel ")
 
 },
+
+{
+	latitude: -8.82704,
+	longitude: 13.243614,
+	title: "Largo da Independência de Angola",
+	description: "Largo da Independência",
+	//content: content("Largo da Independência de Angola")
+},
+
+
 {
 	latitude: -8.8105614,
 	longitude: 13.2340159,
-	title: "National Banc"
+	title: "Banco Nacional de Angola",
+	description: "Banco Nacional de Angola",
+	//content: content("Banco Nacional de Angola")
 },
 {
 	latitude: -8.824754,
 	longitude: 13.2382359,
-	title: "Sagrada Família"
+	title: "Igreja da Sagrada Família",
+	description: "Sagrada Família Luanda Angola",
+	//content: content("Igreja da Sagrada Família")
 },
 {
 	latitude: -8.7645772,
 	longitude: 13.2584397,
-	title: "Coconuts Restaurante"	
+	title: "Coconuts Restaurante",
+	description: "Coconuts Restaurante Luanda Angola",
+	//content: content("Coconuts Restaurante")
 }];
+/*
+function content(title) {
+	var x;
+	$.ajax({
+		url:'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=1b5ad63f3251fe5249199e1a3d291987&text=%22'+ title +'%22&format=json&nojsoncallback=1',
+		async: 'false',
+		success: function(data){ 
+			
+			infoWindow.setContent('<IMG BORDER="0" ALIGN="Left" src= "https://farm' + data.photos.photo[0].farm + '.staticflickr.com/' + data.photos.photo[0].server + '/' + data.photos.photo[0].id + '_' + data.photos.photo[0].secret + '.jpg">');
+				console.log(x);
+				return x;
+		}
+	});
+}
+*/
+
 // Initializing variables to store the map data
 var map;
-var markers= [];
+var allMarkers= [];
+var infoWindow;
 var length = MyLocations.length;
 var clearMarkers;
 var setMapOnAll;
-var setMapOn;
-var currentMarkers= [];
 
 
 //Initializing the map...
 function initMap() {
-      map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -8.8105614, lng: 13.2340159},
+    map = new google.maps.Map(document.getElementById('map'), {
+	    center: {lat: -8.8105614, lng: 13.2340159},
         zoom: 15
+    });
+}
 
 
-      });
-//Sets all markers down on the map
+//Sets all Markers down on the map
 setMapOnAll = function(map) {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
+//Inserts the data from the array MyLocations into the a allMarkers array		
+	clearMarkers();
+	for (var i = 0; i < length; i++) {
+		addMarkerWithTimeout({lat: MyLocations[i].latitude, lng: MyLocations[i].longitude }, MyLocations[i].title, MyLocations[i].content, 300 * i);	
+	}
 }
 
-//Sets current selected array of markers on the map
-setMapOn = function (map) {
-  for (var i = 0; i < currentMarkers.length; i++) {
-    currentMarkers[i].setMap(map);
-  }
+function toggleBounce() {
+	var self = this;
+	if (this.getAnimation() !== null) {
+    	this.setAnimation(null);
+	} 
+	else {
+    	this.setAnimation(google.maps.Animation.BOUNCE);
+    	setTimeout(function() {
+    			self.setAnimation(null);
+    	}, 2100);
+	}
 }
-for (var i = 0; i < length; i++) {
-	markers[i] = Marker = new google.maps.Marker({
-    position:{lat: MyLocations[i].latitude, lng: MyLocations[i].longitude} ,
-    map: map,
-    title: MyLocations[i].title
-	});
-}
-//Cleans the map of markers
+	
+
+	//Sets current selected array of allMarkers on the map
+
+
+
+//Cleans the map of allMarkers
 clearMarkers = function() {
-  setMapOnAll(null);
+	if (allMarkers.length > 0){
+		for (var i = 0; i < length; i++) {
+		    allMarkers[i].setMap(null);
+	   	}
+	}	
 };
+
+function addMarkerWithTimeout(position, title, content, timeout) {
+	window.setTimeout(function() {
+    	
+ 
+    	var marker = (new google.maps.Marker({
+			position: position,
+			map: map,
+			animation: google.maps.Animation.DROP,
+			title: title,
+			content: content
+		}));
+
+		
+		infoWindow = new google.maps.InfoWindow({
+    		content: null 
+ 		});
+		marker.addListener('click', toggleBounce);
+		marker.addListener('click', ViewModel.Info);
+  		allMarkers.push(marker);
+	}, timeout);
 }
 
-   
+setTimeout(function() {
+	setMapOnAll(map);
+}, 2000);
+
